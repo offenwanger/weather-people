45423081eb3df02735b17f532f7892d9b4da16f8
Mohi <mohireza@outlook.com>
2019-03-18 18:22:40 -0700

    Fix file structure

--- Chat-App-Test/app.js
+++ Chat-App-Test/app.js
@@ -4,22 +4,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/local_files/index.html');
 });
 
-app.get('/typer', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/typer', function (req, res) {
+  res.sendFile(__dirname + '/local_files/typer.html');
 });
 
-app.use('/', express.static(__dirname + '/local'));
+app.use('/', express.static(__dirname + '/local_files'));
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
     io.emit('chat message', msg);
   });
 });
 
-http.listen(port, function(){
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
