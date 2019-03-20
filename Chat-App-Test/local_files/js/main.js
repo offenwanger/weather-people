91421cafc3a1826f12b903a124f973cdee33a331
Mohi <mohireza@outlook.com>
2019-03-20 02:23:11 -0700

    add rating system

--- Chat-App-Test/local_files/js/main.js
+++ Chat-App-Test/local_files/js/main.js
@@ -1,1 +1,42 @@
-alert("Hello! I am an alert box!!");
+$(function () {
+
+    // alert("Hello! I am an alert box!!");
+    $(document).ready(function () {
+        $(window).on('load', function () {
+            $("#myModal").modal({
+                backdrop: 'static',
+            });
+        });
+
+        $("#feedback-button").click(function () {
+            $("#myModal1").modal();
+        });
+    });
+
+
+
+
+    $('#record-button').addClass("notRec");
+
+    $('#record-button').click(function () {
+        if ($('#record-button').hasClass('notRec')) {
+            $('#record-button').removeClass("notRec");
+            $('#record-button').addClass("Rec");
+        }
+        else {
+            $('#record-button').removeClass("Rec");
+            $('#record-button').addClass("notRec");
+        }
+    });
+
+    $('.kv-ltr-theme-fas-star').rating({
+        hoverOnClear: false,
+        theme: 'krajee-fas',
+        containerClass: 'is-star'
+    });
+
+    $('#input-1-ltr-star-md').on('rating:change', function (event, value, caption) {
+        console.log(value);
+        console.log(caption);
+    });
+});
